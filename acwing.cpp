b64c6f6852187ffd92cc410e3d9e80643a871355
jikong <jikong_2zzh@qq.com>
2024-03-23 19:54:14 +0800

    第 148 场周赛

---
+++ acwing.cpp
@@ -0,0 +1,16 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a[105];
+//     int n, k;
+//     cin >>n >> k;
+//     for (int i = 1; i <= n;i++){
+//         cin >> a[i];
+
+//     }
+//     cout << a[k];
+//     return 0;
+// }
+
+
+
