a89b58ade297886172e56fd2630b28ad8961d258
jikong <jikong_2zzh@qq.com>
2024-04-21 20:53:36 +0800

    小白不提

---
+++ xiaobaibu.cpp
@@ -0,0 +1,52 @@
+// #include<iostream>
+// #include<algorithm>
+// #include<cstring>
+// #include<vector>
+// #include<set>
+// #include<map>
+// #include<queue>
+// #include<deque>
+// #include<unordered_map>
+// using namespace std;
+// using LL = long long;
+// using ULL = unsigned long long;
+// const int N = 505;
+// int n, m, k;
+// int main(){
+//     ios::sync_with_stdio(false);
+//     cin.tie(0);
+//     cout.tie(0);
+//     cin >> n >> m >> k;
+//     int ans=0;
+//     while(k--){
+//         int a, b;
+//         cin >> a >> b;
+//         int t1 = abs(n / 2 + 1 - a) + abs(m / 2 + 1 - b);
+//         int t2 = max(n / 2, m / 2);
+//         if(t1<=t2)
+//             ans++;
+//     }
+//     cout << ans;
+//     return 0;
+// }
+
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <deque>
+#include <unordered_map>
+using namespace std;
+using LL = long long;
+using ULL = unsigned long long;
+const int N = 505;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    return 0;
+}
