6c40803bff968ed113d3c9fb059706db4f8da562
jikong <jikong_2zzh@qq.com>
2024-05-28 22:21:18 +0800

    牛客round44补题

---
+++ nkround44.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_map>
+#define inf 0x3f3f3f3f
+using namespace std;
+using LL = long long;
+using ULL = unsigned long long;
+typedef pair<int, int> PII;
+typedef pair<double, int> PDI;
+typedef pair<PII, int> PIII;
+const int N = 1e6 + 10;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    return 0;
+}
