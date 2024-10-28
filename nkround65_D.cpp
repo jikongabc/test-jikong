8c51db2a0fe1b6ec7476c0368f741f2d4713a661
jikong <jikong_2zzh@qq.com>
2024-10-28 13:05:49 +0800

    牛客周赛round65

---
+++ nkround65_D.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <bitset>
+#include <numeric>
+#include <cmath>
+#define inf 0x3f3f3f3f
+using namespace std;
+using LL = long long;
+using ULL = unsigned long long;
+typedef pair<int, int> PII;
+typedef pair<int, PII> PIII;
+const int N =  ;
+
+
+int main(){
+	ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	
+	return 0;
+}
